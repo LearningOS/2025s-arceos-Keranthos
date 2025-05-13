2f7ffac53382beddd3716fb13639e59408e44a58
ShanJHS2 <wanwj7@mail2.sysu.edu.cn>
2025-05-13 09:45:24 +0800

    02

--- arceos/ulib/axstd/src/collections/mod.rs
+++ arceos/ulib/axstd/src/collections/mod.rs
@@ -1,4 +1,13 @@
 mod hashmap;
 
+#[cfg(feature = "alloc")]
+pub use alloc::collections::{
+    binary_heap::BinaryHeap,
+    btree_map::BTreeMap,
+    btree_set::BTreeSet,
+    linked_list::LinkedList,
+    vec_deque::VecDeque,
+};
+
 #[doc(hidden)]
 pub use self::hashmap::HashMap;
